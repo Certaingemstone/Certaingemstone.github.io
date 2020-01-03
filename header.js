d0043f6bf1015f8cb494629490cbe978eab7eda4
Jade Chongsathapornpong <jade.chongsathapornpong@gmail.com>
2020-01-02 17:30:57 -0800

    Contact page drafted

---
+++ header.js
@@ -0,0 +1,17 @@
+document.getElementById("NAVMENU").innerHTML =
+'<a class="navbar-brand" href="#">Home</a>' +
+'<button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +
+  '<span class="navbar-toggler-icon"></span>' +
+'</button>' +
+'<div class="collapse navbar-collapse " id="navbarSupportedContent">     <ul class="navbar-nav mr-4">' +
+  '<li class="nav-item">' +
+    '<a class="nav-link" data-value="about" href="about.html">About</a>        </li>' +
+    '<li class="nav-item">' +
+      '<a class="nav-link" data-value="projects" href="projects.html">Projects</a>' +
+    '</li>' +
+    '<li class="nav-item">' +
+      '<a class="nav-link " data-value="blog" href="blog.html">Blog</a>         </li>' +
+      '<li class="nav-item">' +
+          '<a class="nav-link " data-value="contact" href="contact.html">Contact</a>       </li>' +
+        '</ul>' +
+'</div>';

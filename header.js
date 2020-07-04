cca75e4be5a4a7e115e9d4c9f229ace5a3a44c27
Jade Chongsathapornpong <jade.chongsathapornpong@gmail.com>
2020-07-04 00:35:49 -0700

    Update
    Updated background images and text. Implemented page fade in on load.

--- header.js
+++ header.js
@@ -5,15 +5,16 @@ document.getElementById("NAVMENU").innerHTML =
 '</button>' +
 '<div class="collapse navbar-collapse " id="navbarSupportedContent">     <ul class="navbar-nav mr-4">' +
   '<li class="nav-item">' +
-    '<a class="nav-link" data-value="about" href="about.html">About</a>        </li>' +
+    '<a class="nav-link" data-value="about" href="about.html">About</a>        </li>'
+    +
     '<li class="nav-item">' +
       '<a class="nav-link" data-value="gallery" href="gallery.html">Gallery</a>' +
     '</li>' +
+
     '<li class="nav-item">' +
       '<a class="nav-link" data-value="projects" href="projects.html">Projects</a>' +
     '</li>' +
-    '<li class="nav-item">' +
-      '<a class="nav-link " data-value="blog" href="blog.html">Blog</a>         </li>' +
+
       '<li class="nav-item">' +
           '<a class="nav-link " data-value="contact" href="contact.html">Contact</a>       </li>' +
         '</ul>' +

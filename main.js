a6ac6e0aab1233fbd6f66bad910fb82fe44f98ad
Jade Chongsathapornpong <jade.chongsathapornpong@gmail.com>
2022-12-28 13:17:41 -0800

    Redesign homepage

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-$(document).ready(function(){
- $('.header').height($(window).height());
+const menu = document.getElementById("menu");
 
-})
+Array.from(document.getElementsByClassName("menu-item"))
+  .forEach((item, index) => {
+    item.onmouseover = () => {
+      menu.dataset.activeIndex = index;
+    }
+  });
